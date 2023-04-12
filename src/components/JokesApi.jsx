4e1fc06734d9d78b69623a94e79397c33a1ef2c1
Olamide Alao <olamidealao22@gmail.com>
2023-04-12 11:01:46 +0100

    new updates

--- src/components/JokesApi.jsx
+++ src/components/JokesApi.jsx
@@ -24,12 +24,12 @@ const jokesApi = () => {
   return (
     <>
       <section className='col-lg-5 m-auto'>
-          <h2 className='text-center fw-bold' style={{color: "red"}}>Jokes</h2>
+          <h1 className='text-center fw-bold' style={{color: "red"}}>Jokes</h1>
         <div className='bg-prima rounded ps-2'>
-          <h3 className='fs-2 text-center'>{response.category}</h3>
-          <p className='fs-4'>{response.joke}</p>
+          <h3 className='fs-2 text-center' style={{color: "red"}}>{response.category}</h3>
+          <p className='fs-4' style={{color: "#363635"}}>{response.joke}</p>
        <div className="text-center">
-       <button onClick={fetchJokes} className='btn btn-outline-warning text-white mt-2 col-7'>Get Jokes</button>
+       <button onClick={fetchJokes} className='btn btn-outline-warning text-white mt-2 col-7 mb-3'>Get Jokes</button>
        </div>
         </div>
 
